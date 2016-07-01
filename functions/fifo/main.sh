a16b32d9c80873f201f60efdfd5fdb3a205e94ea
Sam Shelton <sam@shelt.ca>
2016-06-30 19:16:40 -0500

    Fix base install

--- functions/fifo/main.sh
+++ functions/fifo/main.sh
@@ -41,10 +41,12 @@ install_installer()
 
 install_base()
 {
+    ( cd "$CHROOT_DIR" && mkdir -p bin boot cdrom dev etc home lib lib64 media mnt opt proc root sbin sys tmp usr var )
     local tagfiles="$(find "$PKG_DIR" -type f | grep "tagfile$")"
-    local BASE= "$(egrep ':ADD$' $tagfiles | cut -f2 -d:)"          # All required packages
-    local BASE+="$(egrep ':REC$' $PKG_DIR/l/tagfile | cut -f2 -d:)" # Recommended libraries
-    BASE="$(echo "$BASE" | sed '/kernel-huge/c\kernel-generic')"    # Use generic kernel
+    local BASE
+    BASE=" $(egrep ':ADD$' $tagfiles | cut -f2 -d:)"              # All required packages
+    BASE=" $(echo "$BASE" | sed '/kernel-huge/c\kernel-generic')" # Use generic kernel
+    BASE+=" $(egrep ':REC$' $PKG_DIR/l/tagfile | cut -f1 -d:)"     # Recommended libraries
     # Lilo deps
     BASE+=" slackpkg"     # Installing desired packages
     BASE+=" ncurses"      # I forget
@@ -65,14 +67,14 @@ install_base()
     BASE+=" diffutils"    # I forget
     BASE+=" rsync"        # sbopkg
     #TODO this is not all that is needed
-
+    
     local BASE_FNAMES="$(pkg_to_fname $BASE)"
     [ -n "$BASE_FNAMES" ] || fatal "Failed to generate base installation package list"
-    installpkg --root "$CHROOT_DIR" $BASE_FNAMES >/dev/null
+    installpkg --root "$CHROOT_DIR" $BASE_FNAMES #TODO >/dev/null
     
     mkdir  -p "/tmp"
-    wget --no-check-certificate "https://github.com/sbopkg/sbopkg/releases/download/0.37.1/sbopkg-0.37.1-noarch-1_wsr.tgz" -O "/tmp/sbopkg.tgz"
-    installpkg --root "$CHROOT_DIR" "/tmp/sbopkg.tgz"
+    wget --no-check-certificate "https://github.com/sbopkg/sbopkg/releases/download/0.37.1/sbopkg-0.37.1-noarch-1_wsr.tgz" -O "/tmp/sbopkg.tgz" >/dev/null
+    installpkg --root "$CHROOT_DIR" "/tmp/sbopkg.tgz" >/dev/null
     
 }
 
@@ -87,7 +89,7 @@ do_chroot()
 pkg_to_fname()
 {
     for curr in $@; do
-        find "$PKG_DIR" -type f | egrep "/$curr-.*\.(txz|tgz)$"
+        find "$PKG_DIR" -type f | egrep "/.*\.(txz|tgz)$" | fgrep "$curr"
         [ $? -eq 1 ] && error "Failed to locate tarball for package $curr"
     done
 }
